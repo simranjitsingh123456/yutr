6f43cc7446121242525fff0e763b596501f7af7a
simranjit singh <30011264@student.toiohomai.ac.nz>
2019-03-13 10:23:33 +1300

    msg

---
+++ odd/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            Console.WriteLine("please enter a number and i will tell you if its odd or even");
+            num1 = int.Parse(Console.ReadLine());
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine("your number is even");
+            }
+            else
+            {
+                Console.WriteLine("your number is odd");
+            }
+            Console.ReadLine();
+        }
+    }
+}
